04c14c329a0599a693379283f7e10e29860f44ca
dessanagit <dessanacms@gmail.com>
2020-05-16 22:49:23 -0400

    Apply Random()

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -29,17 +31,21 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         child: FlatButton(
           onPressed: () {
-            setState(() {
-              print('I got Clicked!');
-            });
+            setState(
+              () {
+                ballNumber = Random().nextInt(5) + 1;
+                print('I got Clicked!');
+              },
+            );
           },
-          child: Image.asset('images/ball1.png'),
+          child: Image.asset('images/ball$ballNumber.png'),
         ),
       ),
     );
